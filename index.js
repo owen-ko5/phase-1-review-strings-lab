dee867f5d2cbfb9ff6a61ce7a08f3a7b1aee2aa5
Owen <owenkaris9512@gmail.com>
2025-03-06 18:57:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Owen Kariuki'
+const welcomeMessage = 'Welcome to Flatbook,  Owen Kariuki {currentUser}!';
+const excitedWelcomeMessage= 'WELCOME TO FLATBOOK, Owen Kariuki {OWEN KARIUKI}!';
+const shortGreeting = 'Welcome, O {OWEN KARIUKI}!';
